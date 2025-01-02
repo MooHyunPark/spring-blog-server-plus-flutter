989915d5ce7f9c639c8d1dbb6186d73eccade531
angus <angus1208@naver.com>
2025-01-02 14:34:36 +0900

    [feat] jar파일 추가 및 ip 주소 변경

--- lib/data/gm/session_gm.dart
+++ lib/data/gm/session_gm.dart
@@ -2,29 +2,42 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_blog/main.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SessionGM {
+// 로그인을 하면 username, token 등등을 집어넣고 isLogin은 true로 변경
+class SessionUser {
   int? id;
   String? username;
   String? accessToken;
   bool? isLogin;
 
-  SessionGM({this.id, this.username, this.accessToken, this.isLogin = false});
+  SessionUser({this.id, this.username, this.accessToken, this.isLogin});
+}
 
+class SessionGM extends Notifier<SessionUser> {
+  // TODO 2: 모름
   final mContext = navigatorKey.currentContext!;
 
+  @override
+  SessionUser build() {
+    return SessionUser(
+        id: null, username: null, accessToken: null, isLogin: false);
+  }
+
   Future<void> login() async {}
+
   Future<void> join() async {}
+
   Future<void> logout() async {}
+
   Future<void> autoLogin() async {
     Future.delayed(
       Duration(seconds: 3),
       () {
-        Navigator.popAndPushNamed(mContext, "/post/list");
+        Navigator.popAndPushNamed(mContext, "/login");
       },
     );
   }
 }
 
-final sessionProvider = StateProvider<SessionGM>((ref) {
+final sessionProvider = NotifierProvider<SessionGM, SessionUser>(() {
   return SessionGM();
 });
